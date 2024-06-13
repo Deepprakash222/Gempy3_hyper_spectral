7e3dcf6c68ced983213694e7ffc69a7e5ec6238e
jt925938 <jt925938@login23-3.hpc.itc.rwth-aachen.de>
2024-06-13 11:32:07 +0200

    modified the run.py and added a bash script along with a Result folder to store the result

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ from sklearn.mixture import BayesianGaussianMixture
 from sklearn.cluster import KMeans
 
 
-    
+
 def main():
     
     # Load .mat file
@@ -38,7 +38,7 @@ def main():
     # Arrange the label in groundtruth
     i=0
     label_data = [0,6,1,5,4,3,2]
-    print(np.unique(SalinasA_gt))
+    
     for ele in np.unique(SalinasA_gt):
         mask = SalinasA_gt==ele
         SalinasA_gt[mask] = label_data[i]
@@ -47,12 +47,425 @@ def main():
     ###### TODO################################################################
     ## Arrange Data as concatationation of spacial co-ordinate and pixel values
     ###########################################################################
+    
     column = 20
     y_obs = torch.tensor(SalinasA_gt[:,column], dtype=torch.float64)
     mask = y_obs!=0
     y_obs_label = y_obs[mask]
     y_obs_hsi = torch.tensor(SalinasA_corrected[:,column,:], dtype=torch.float64)[mask]
-    print(y_obs_hsi.shape)
+    
+    normalised_hsi = zscore(y_obs_hsi, axis=1)
+
+    gm =gm = BayesianGaussianMixture(n_components=6, random_state=0).fit(normalised_hsi)
+    
+    gmm_label = gm.predict(normalised_hsi) +1 
+    
+    gmm_label2 = torch.zeros_like(y_obs_label)
+    gmm_label2[gmm_label==2]=6
+    gmm_label2[gmm_label==4]=5
+    gmm_label2[gmm_label==1]=4
+    gmm_label2[gmm_label==3]=3
+    gmm_label2[gmm_label==6]=2
+    gmm_label2[gmm_label==5]=1
+    
+    mean_init, cov_init = gm.means_[[4,5,2,0,3,1]], gm.covariances_[[4,5,2,0,3,1]]
+    
+    geo_model_test = gp.create_geomodel(
+    project_name='Gempy_abc_Test',
+    extent=[0, 86, -10, 10, -83, 0],
+    resolution=[86,20,83],
+    refinement=3,
+    structural_frame= gp.data.StructuralFrame.initialize_default_structure()
+    )
+
+    gp.add_surface_points(
+        geo_model=geo_model_test,
+        x=[70.0, 80.0],
+        y=[0.0, 0.0],
+        z=[-77.0, -71.0],
+        elements_names=['surface1', 'surface1']
+    )
+
+    gp.add_orientations(
+        geo_model=geo_model_test,
+        x=[75],
+        y=[0.0],
+        z=[-74],
+        elements_names=['surface1'],
+        pole_vector=[[-5/3, 0, 1]]
+    )
+    geo_model_test.update_transform(gp.data.GlobalAnisotropy.NONE)
+
+    element2 = gp.data.StructuralElement(
+        name='surface2',
+        color=next(geo_model_test.structural_frame.color_generator),
+        surface_points=gp.data.SurfacePointsTable.from_arrays(
+            x=np.array([20.0, 60.0]),
+            y=np.array([0.0, 0.0]),
+            z=np.array([-74, -52]),
+            names='surface2'
+        ),
+        orientations=gp.data.OrientationsTable.initialize_empty()
+    )
+
+    geo_model_test.structural_frame.structural_groups[0].append_element(element2)
+
+    element3 = gp.data.StructuralElement(
+        name='surface3',
+        color=next(geo_model_test.structural_frame.color_generator),
+        surface_points=gp.data.SurfacePointsTable.from_arrays(
+            x=np.array([0.0, 30.0, 60]),
+            y=np.array([0.0, 0.0,0.0]),
+            z=np.array([-72, -55.5, -39]),
+            names='surface3'
+        ),
+        orientations=gp.data.OrientationsTable.initialize_empty()
+    )
+
+    geo_model_test.structural_frame.structural_groups[0].append_element(element3)
+
+    element4 = gp.data.StructuralElement(
+        name='surface4',
+        color=next(geo_model_test.structural_frame.color_generator),
+        surface_points=gp.data.SurfacePointsTable.from_arrays(
+            x=np.array([0.0, 20.0, 60]),
+            y=np.array([0.0, 0.0,0.0]),
+            z=np.array([-61, -49, -27]),
+            names='surface4'
+        ),
+        orientations=gp.data.OrientationsTable.initialize_empty()
+    )
+
+    geo_model_test.structural_frame.structural_groups[0].append_element(element4)
+
+    element5 = gp.data.StructuralElement(
+        name='surface5',
+        color=next(geo_model_test.structural_frame.color_generator),
+        surface_points=gp.data.SurfacePointsTable.from_arrays(
+            x=np.array([0.0, 20.0, 40]),
+            y=np.array([0.0, 0.0, 0.0]),
+            z=np.array([-39, -28, -16]),
+            names='surface5'
+        ),
+        orientations=gp.data.OrientationsTable.initialize_empty()
+    )
+
+    geo_model_test.structural_frame.structural_groups[0].append_element(element5)
+
+    element6 = gp.data.StructuralElement(
+        name='surface6',
+        color=next(geo_model_test.structural_frame.color_generator),
+        surface_points=gp.data.SurfacePointsTable.from_arrays(
+            x=np.array([0.0, 20.0,30]),
+            y=np.array([0.0, 0.0, 0.0]),
+            z=np.array([-21, -10, -1]),
+            names='surface6'
+        ),
+        orientations=gp.data.OrientationsTable.initialize_empty()
+    )
+
+    geo_model_test.structural_frame.structural_groups[0].append_element(element6)
+
+    geo_model_test.structural_frame.structural_groups[0].elements[0], geo_model_test.structural_frame.structural_groups[0].elements[1],\
+    geo_model_test.structural_frame.structural_groups[0].elements[2], geo_model_test.structural_frame.structural_groups[0].elements[3],\
+    geo_model_test.structural_frame.structural_groups[0].elements[4], geo_model_test.structural_frame.structural_groups[0].elements[5] = \
+    geo_model_test.structural_frame.structural_groups[0].elements[1], geo_model_test.structural_frame.structural_groups[0].elements[0],\
+    geo_model_test.structural_frame.structural_groups[0].elements[3], geo_model_test.structural_frame.structural_groups[0].elements[2],\
+    geo_model_test.structural_frame.structural_groups[0].elements[5], geo_model_test.structural_frame.structural_groups[0].elements[4]  
+
+    gp.compute_model(geo_model_test)
+    
+    y_obs_label = 7 -y_obs[mask]
+    
+    x_loc = 20
+    y_loc = 0
+    z_loc = np.linspace(0,-82, 83)
+    xyz_coord = np.array([[x_loc, y_loc, z] for z in z_loc])[mask]
+    gp.set_custom_grid(geo_model_test.grid, xyz_coord=xyz_coord)
+    
+    geo_model_test.interpolation_options.mesh_extraction = False
+    sol = gp.compute_model(geo_model_test)
+    
+    sp_coords_copy_test = geo_model_test.interpolation_input.surface_points.sp_coords.copy()
+    geo_model_test.transform.apply_inverse(sp_coords_copy_test)
+    
+    gp.compute_model(geo_model_test)
+    
+    sp_coords_copy_test = geo_model_test.interpolation_input.surface_points.sp_coords.copy()
+    geo_model_test.transform.apply_inverse(sp_coords_copy_test)
+    
+    # Change the backend to PyTorch for probabilistic modeling
+    BackendTensor.change_backend_gempy(engine_backend=gp.data.AvailableBackends.PYTORCH)
+    
+    # geo_model_test.interpolation_options.uni_degree = 0
+    # geo_model_test.interpolation_options.mesh_extraction = False
+    geo_model_test.interpolation_options.sigmoid_slope = 50
+    
+    
+    @config_enumerate
+    def model_test(y_obs_label):
+        """
+        This Pyro model represents the probabilistic aspects of the geological model.
+        It defines a prior distribution for the top layer's location and
+        computes the thickness of the geological layer as an observed variable.
+        """
+        # Define prior for the top layer's location
+        prior_mean_surface_1 = sp_coords_copy_test[11, 2]
+        prior_mean_surface_2 = sp_coords_copy_test[14, 2]
+        prior_mean_surface_3 = sp_coords_copy_test[5, 2]
+        prior_mean_surface_4 = sp_coords_copy_test[0, 2]
+
+        mu_surface_1 = pyro.sample('mu_1', dist.Normal(prior_mean_surface_1, torch.tensor(0.2, dtype=torch.float64)))
+        mu_surface_2 = pyro.sample('mu_2', dist.Normal(prior_mean_surface_2, torch.tensor(0.2, dtype=torch.float64)))
+        mu_surface_3 = pyro.sample('mu_3', dist.Normal(prior_mean_surface_3, torch.tensor(0.2, dtype=torch.float64)))
+        mu_surface_4 = pyro.sample('mu_4', dist.Normal(prior_mean_surface_4, torch.tensor(0.2, dtype=torch.float64)))
+        #print(mu_surface_1, mu_surface_2)
+        # Ensure that mu_surface_1 is greater than mu_surface_2
+        pyro.sample('mu_1 < 0', dist.Delta(torch.tensor(1.0, dtype=torch.float64)), obs=(mu_surface_1 < 3.7))
+        pyro.sample('mu_1 > mu_2', dist.Delta(torch.tensor(1.0, dtype=torch.float64)), obs=(mu_surface_1 > mu_surface_2))
+        pyro.sample('mu_2 > -38.5', dist.Delta(torch.tensor(1.0, dtype=torch.float64)), obs=(mu_surface_2 > 1.775))
+        pyro.sample('mu_3 < -38.5', dist.Delta(torch.tensor(1.0, dtype=torch.float64)), obs=(mu_surface_3 < 1.4))
+        pyro.sample('mu_3 > -61.5', dist.Delta(torch.tensor(1.0, dtype=torch.float64)), obs=(mu_surface_3 > 0.625))
+        pyro.sample('mu_4 < -61.5', dist.Delta(torch.tensor(1.0, dtype=torch.float64)), obs=(mu_surface_4 < 0.625))
+        pyro.sample('mu_4 > -83', dist.Delta(torch.tensor(1.0, dtype=torch.float64)), obs=(mu_surface_4 > - 0.2 ))
+        # Update the model with the new top layer's location
+        interpolation_input = geo_model_test.interpolation_input
+        
+        
+        interpolation_input.surface_points.sp_coords = torch.index_put(
+            interpolation_input.surface_points.sp_coords,
+            (torch.tensor([11]), torch.tensor([2])),
+            mu_surface_1
+        )
+        interpolation_input.surface_points.sp_coords = torch.index_put(
+            interpolation_input.surface_points.sp_coords,
+            (torch.tensor([14]), torch.tensor([2])),
+            mu_surface_2
+        )
+        
+        interpolation_input.surface_points.sp_coords = torch.index_put(
+            interpolation_input.surface_points.sp_coords,
+            (torch.tensor([5]), torch.tensor([2])),
+            mu_surface_3
+        )
+        interpolation_input.surface_points.sp_coords = torch.index_put(
+            interpolation_input.surface_points.sp_coords,
+            (torch.tensor([0]), torch.tensor([2])),
+            mu_surface_4
+        )
+        
+        
+        #print("interpolation_input",interpolation_input.surface_points.sp_coords)
+        
+        # # Compute the geological model
+        geo_model_test.solutions = gempy_engine.compute_model(
+            interpolation_input=interpolation_input,
+            options=geo_model_test.interpolation_options,
+            data_descriptor=geo_model_test.input_data_descriptor,
+            geophysics_input=geo_model_test.geophysics_input,
+        )
+        
+        # Compute and observe the thickness of the geological layer
+        
+        custom_grid_values = geo_model_test.solutions.octrees_output[0].last_output_center.custom_grid_values
+        lambda_ = 10.0
+        loc_mean = torch.tensor(mean_init,dtype=torch.float64)
+        loc_cov =  torch.tensor(cov_init, dtype=torch.float64)
+        class_label = F.softmax(-lambda_* (torch.tensor([1,2,3,4,5,6], dtype=torch.float64) - custom_grid_values.reshape(-1,1))**2, dim=1)
+        sample =[]
+        for i in range(loc_mean.shape[0]):
+            sample_data = pyro.sample("sample_data"+str(i+1), dist.MultivariateNormal(loc=loc_mean[i],covariance_matrix=loc_cov[i]))
+            sample.append(sample_data)
+        sample_tesnor = torch.stack(sample, dim=0)
+        
+        with pyro.plate('N='+str(y_obs_label.shape[0]), y_obs_label.shape[0]):
+            assignment = pyro.sample("assignment", dist.Categorical(class_label))
+            #print("mean\n", mean[assignment])
+            #obs = pyro.sample("obs", dist.Normal(custom_grid_values, 0.5 ), obs=y_obs_label.reshape(-1))
+            #obs = pyro.sample("obs", dist.Normal(sample[assignment], 0.1 ), obs=y_obs_label.reshape(-1))
+            #print(sample_tesnor[assignment].shape, loc_cov[assignment].shape, y_obs_label.shape)
+            obs = pyro.sample("obs", dist.MultivariateNormal(loc=sample_tesnor[assignment],covariance_matrix=loc_cov[assignment]), obs=y_obs_label)
+            #obs = pyro.sample("obs", dist.Normal(mean[assignment],  sigma[assignment]), obs=y_obs_label.reshape(-1))
+        #return obs
+        
+    pyro.set_rng_seed(42)
+
+    prior = Predictive(model_test, num_samples=100)(normalised_hsi)
+
+    # Key to avoid
+    avoid_key = ['mu_1 < 0','mu_1 > mu_2','mu_2 > -38.5', 'mu_3 < -38.5','mu_3 > -61.4','mu_4 < -61.5', 'mu_4 > -83']
+
+    # Create sub-dictionary without the avoid_key
+    prior = dict((key, value) for key, value in prior.items() if key not in avoid_key)
+
+    data = az.from_pyro(prior=prior)
+    
+    pyro.primitives.enable_validation(is_validate=True)
+    nuts_kernel = NUTS(model_test, step_size=0.0085, adapt_step_size=True, target_accept_prob=0.9, max_tree_depth=10, init_strategy=init_to_mean)
+    #nuts_kernel = NUTS(model_test, step_size=0.00085, adapt_step_size=True, target_accept_prob=0.9, max_tree_depth=10)
+    #nuts_kernel = NUTS(model_test)
+    #initial_values = {'mu_1': torch.tensor(0.01, dtype=torch.float64),'mu_2': torch.tensor(0.01, dtype=torch.float64) }
+    initial_values = {'mu_1': torch.tensor(0.01, dtype=torch.float64),'mu_2': torch.tensor(0.01, dtype=torch.float64) }
+    #mcmc = MCMC(nuts_kernel, num_samples=200, warmup_steps=50, disable_validation=False, initial_params=initial_values)
+    mcmc = MCMC(nuts_kernel, num_samples=50, warmup_steps=50, disable_validation=False)
+    mcmc.run(normalised_hsi)
+    
+    posterior_samples = mcmc.get_samples()
+    posterior_predictive = Predictive(model_test, posterior_samples)(normalised_hsi)
+    data = az.from_pyro(posterior=mcmc, prior=prior, posterior_predictive=posterior_predictive)
+    
+    mu_1_mean = posterior_samples["mu_1"].mean()
+    mu_2_mean = posterior_samples["mu_2"].mean()
+    mu_3_mean = posterior_samples["mu_3"].mean()
+    mu_4_mean = posterior_samples["mu_4"].mean()
+    
+    # # Update the model with the new top layer's location
+    interpolation_input = geo_model_test.interpolation_input
+    interpolation_input.surface_points.sp_coords = torch.index_put(
+        interpolation_input.surface_points.sp_coords,
+        (torch.tensor([11]), torch.tensor([2])),
+        posterior_samples["mu_1"].mean()
+    )
+    interpolation_input.surface_points.sp_coords = torch.index_put(
+        interpolation_input.surface_points.sp_coords,
+        (torch.tensor([14]), torch.tensor([2])),
+        posterior_samples["mu_2"].mean()
+    )
+
+    interpolation_input.surface_points.sp_coords = torch.index_put(
+            interpolation_input.surface_points.sp_coords,
+            (torch.tensor([5]), torch.tensor([2])),
+            posterior_samples["mu_3"].mean()
+        )
+    interpolation_input.surface_points.sp_coords = torch.index_put(
+            interpolation_input.surface_points.sp_coords,
+            (torch.tensor([0]), torch.tensor([2])),
+            posterior_samples["mu_4"].mean()
+        )
+        
+    #print("interpolation_input",interpolation_input.surface_points.sp_coords)
+
+    # # Compute the geological model
+    geo_model_test.solutions = gempy_engine.compute_model(
+        interpolation_input=interpolation_input,
+        options=geo_model_test.interpolation_options,
+        data_descriptor=geo_model_test.input_data_descriptor,
+        geophysics_input=geo_model_test.geophysics_input,
+    )
+    
+    sp_coords_copy_test2 =interpolation_input.surface_points.sp_coords
+    
+    sp_cord= geo_model_test.transform.apply_inverse(sp_coords_copy_test2.detach().numpy())
+    
+    print("co_ordinates\n",sp_coords_copy_test)
+    
+    geo_model_test_post = gp.create_geomodel(
+    project_name='Gempy_abc_Test_post',
+    extent=[0, 86, -10, 10, -83, 0],
+    resolution=[86,20,83],
+    refinement=7,
+    structural_frame= gp.data.StructuralFrame.initialize_default_structure()
+    )
+
+    gp.add_surface_points(
+        geo_model=geo_model_test_post,
+        x=[70.0, 80.0],
+        y=[0.0, 0.0],
+        z=[-77.0, -71.0],
+        elements_names=['surface1', 'surface1']
+    )
+
+    gp.add_orientations(
+        geo_model=geo_model_test_post,
+        x=[75],
+        y=[0.0],
+        z=[-74],
+        elements_names=['surface1'],
+        pole_vector=[[-5/3, 0, 1]]
+    )
+    geo_model_test_post.update_transform(gp.data.GlobalAnisotropy.NONE)
+
+    element2 = gp.data.StructuralElement(
+        name='surface2',
+        color=next(geo_model_test_post.structural_frame.color_generator),
+        surface_points=gp.data.SurfacePointsTable.from_arrays(
+            x=np.array([20.0, 60.0]),
+            y=np.array([0.0, 0.0]),
+            z=np.array([sp_cord[0,2], -52]),
+            names='surface2'
+        ),
+        orientations=gp.data.OrientationsTable.initialize_empty()
+    )
+
+    geo_model_test_post.structural_frame.structural_groups[0].append_element(element2)
+
+    element3 = gp.data.StructuralElement(
+        name='surface3',
+        color=next(geo_model_test_post.structural_frame.color_generator),
+        surface_points=gp.data.SurfacePointsTable.from_arrays(
+            x=np.array([0.0, 30.0, 60]),
+            y=np.array([0.0, 0.0,0.0]),
+            z=np.array([-72, -55.5, -39]),
+            names='surface3'
+        ),
+        orientations=gp.data.OrientationsTable.initialize_empty()
+    )
+
+    geo_model_test_post.structural_frame.structural_groups[0].append_element(element3)
+
+    element4 = gp.data.StructuralElement(
+        name='surface4',
+        color=next(geo_model_test_post.structural_frame.color_generator),
+        surface_points=gp.data.SurfacePointsTable.from_arrays(
+            x=np.array([0.0, 20.0, 60]),
+            y=np.array([0.0, 0.0,0.0]),
+            z=np.array([-61, sp_cord[5,2], -27]),
+            names='surface4'
+        ),
+        orientations=gp.data.OrientationsTable.initialize_empty()
+    )
+
+    geo_model_test_post.structural_frame.structural_groups[0].append_element(element4)
+
+    element5 = gp.data.StructuralElement(
+        name='surface5',
+        color=next(geo_model_test_post.structural_frame.color_generator),
+        surface_points=gp.data.SurfacePointsTable.from_arrays(
+            x=np.array([0.0, 20, 40]),
+            y=np.array([0.0, 0.0, 0.0]),
+            z=np.array([-39, sp_cord[14,2], -16]),
+            names='surface5'
+        ),
+        orientations=gp.data.OrientationsTable.initialize_empty()
+    )
+
+    geo_model_test_post.structural_frame.structural_groups[0].append_element(element5)
+
+    element6 = gp.data.StructuralElement(
+        name='surface6',
+        color=next(geo_model_test_post.structural_frame.color_generator),
+        surface_points=gp.data.SurfacePointsTable.from_arrays(
+            x=np.array([0.0, 20.0,30]),
+            y=np.array([0.0, 0.0, 0.0]),
+            z=np.array([-21, sp_cord[11,2], -1]),
+            names='surface6'
+        ),
+        orientations=gp.data.OrientationsTable.initialize_empty()
+    )
+
+    geo_model_test_post.structural_frame.structural_groups[0].append_element(element6)
+
+    geo_model_test_post.structural_frame.structural_groups[0].elements[0], geo_model_test_post.structural_frame.structural_groups[0].elements[1],\
+    geo_model_test_post.structural_frame.structural_groups[0].elements[2], geo_model_test_post.structural_frame.structural_groups[0].elements[3],\
+    geo_model_test_post.structural_frame.structural_groups[0].elements[4], geo_model_test_post.structural_frame.structural_groups[0].elements[5] = \
+    geo_model_test_post.structural_frame.structural_groups[0].elements[1], geo_model_test_post.structural_frame.structural_groups[0].elements[0],\
+    geo_model_test_post.structural_frame.structural_groups[0].elements[3], geo_model_test_post.structural_frame.structural_groups[0].elements[2],\
+    geo_model_test_post.structural_frame.structural_groups[0].elements[5], geo_model_test_post.structural_frame.structural_groups[0].elements[4]  
+
+
+    gp.set_custom_grid(geo_model_test_post.grid, xyz_coord=xyz_coord)
+    gp.compute_model(geo_model_test_post)
+
     
 if __name__ == "__main__":
     main()
